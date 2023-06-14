0df9625b1b49abc1f0708dc6d0ef50050e198156
lucky kushwaha <luckykushwaha56@gmail.com>
2023-06-15 00:28:36 +0530

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GraphComponent } from './graph/graph.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { StarRatingModule } from 'angular-star-rating';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    StarRatingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/graph/graph.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.css']
+})
+export class GraphComponent implements OnInit {
+  
+  questions: any;
+  newQuestion: any = {};
+  topic="Graph";
+  formError = ''; // Variable to store server error message
+
+  
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  ngOnInit() {
+    this.loadQuestions();
+  }
+
+  loadQuestions() {
+    
+    this.http.get<any[]>('http://localhost:5000/api/graph').subscribe(
+      (data) => {
+        // console.log(data);
+        this.questions = data;
+      },
+      (error) => {
+        console.log('An error occurred:', error);
+      }
+    );
+  }
+
+  addQuestion() {
+    // Clear previous error
+    this.formError = '';
+    this.http.post<any>('http://localhost:5000/api/graph', this.newQuestion)
+      .subscribe(
+        (response) => {
+          console.log('Question added successfully:', response);
+          this.loadQuestions();
+          // Reset the form after successful submission
+          this.newQuestion = {};
+        },
+        (error) => {
+           // Handle error response
+           if (error.error && error.error.errors && error.error.errors.length > 0) {
+            // Extract the first error message
+            this.formError = error.error.errors[0].msg;
+          } else {
+            // Generic error message
+            this.formError = 'An error occurred while adding the question.';
+          }
+          console.log('An error occurred:', error);
+        }
+      );
+  }
+
+  deleteQuestion(id: string) {
+    this.http.delete(`http://localhost:5000/api/graph/${id}`).subscribe(
+      () => {
+        console.log('Question deleted successfully');
+        this.loadQuestions();
+      },
+      (error) => {
+        console.log('An error occurred:', error);
+      }
+    );
+  }
+
+ 
+  
+}
