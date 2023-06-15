6f4c9f0c406b1f78e468c7b8c7d083b55f736166
lucky kushwaha <luckykushwaha56@gmail.com>
2023-06-15 12:06:57 +0530

    nav bar updated

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  ngOnInit() {
+    $(document).ready(() => {
+      $('.navbar-toggler').click(() => {
+        $('.navbar-collapse').toggleClass('show');
+      });
+    });
+  }
 
 }
